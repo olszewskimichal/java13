838dad194ad92ac1a161fa69b4dd7a805b1fe7be
Michal Olszewski <michal.a.olszewski@allegro.pl>
2020-03-17 21:17:02 +0100

    Switch statement changes

---
+++ src/main/java/SwitchExpressions.java
@@ -0,0 +1,16 @@
+public class SwitchExpressions {
+	public static void main(String[] args) {
+		var me = 4;
+		var operation = "squareMe";
+		var result = switch (operation) {
+			case "doubleMe" -> {
+				yield me * 2;
+			}
+			case "squareMe" -> {
+				yield me * me;
+			}
+			default -> me;
+		};
+		System.err.println(result);
+	}
+}
